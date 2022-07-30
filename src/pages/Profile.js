118fdd6e91849c75c276f52f8454d7f7213ac112
simon409 <addarm409@gmail.com>
2022-07-30 16:30:36 +0100

    removing ig feeds

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,6 @@ import DefaultNavbar from "components/DefaultNavbar";
 import DefaultFooter from "components/DefaultFooter";
 import Header from "components/profile/Header";
 import Content from "components/profile/Content";
-import InstagramFeed from "react-ig-feed";
-import "react-ig-feed/dist/index.css";
 
 export default function Profile() {
   return (
@@ -14,10 +12,6 @@ export default function Profile() {
       <main>
         <Header />
         <Content />
-        <InstagramFeed
-          token="IGQVJWeHR3X3pOUkI5eFI2anJIQ1NtSmRoS05pbEw1emNiX2s4ckNDQkVOQ3I3VEsyN1NteC1zcmdYc1UzU0tBcmtOWkZADUHJPMVdDUFY2Y043bWR5RWV5dC1TeE9YSVFJTC1zSlpmNmNDd01OS3FiMQZDZD"
-          counter="1"
-        />
       </main>
       <DefaultFooter />
     </>
